edcad08d727769340ce472c5b9da4a3d31179011
Camila Santos <acamilass@gmail.com>
2018-10-13 20:04:50 -0300

    add images and cards

--- src/app/shared/music-card/music-card.component.ts
+++ src/app/shared/music-card/music-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee, faUmbrella, faMoon, faSun, faUmbrellaBeach, faDrum } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-music-card',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusicCardComponent implements OnInit {
 
-  titles = ['Coffee break', 'Rainy day', 'Party night', 'Sunset', 'Relax at the beach', 'Punk'];
+  cards = [
+    { icon: faCoffee, title: 'Coffee break' },
+    { icon: faUmbrella, title: 'Rainy day' },
+    { icon: faMoon, title: 'Party night' },
+    { icon: faSun, title: 'Sunset' },
+    { icon: faUmbrellaBeach, title: 'Relax at the beach' },
+    { icon: faDrum, title: 'Punk' },
+  ];
+  
+  // titles = ['Coffee break', 'Rainy day', 'Party night', 'Sunset', 'Relax at the beach', 'Punk'];
   
   constructor() { }
 
