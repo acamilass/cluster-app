a09868a133b552d918a31aaeffb4e682242e88e5
Camila Santos <acamilass@gmail.com>
2018-10-13 23:13:14 -0300

    add background image to cards

--- src/app/shared/music-card/music-card.component.ts
+++ src/app/shared/music-card/music-card.component.ts
@@ -9,12 +9,13 @@ import { faCoffee, faUmbrella, faMoon, faSun, faUmbrellaBeach, faDrum } from '@f
 export class MusicCardComponent implements OnInit {
 
   cards = [
-    { icon: '../../../assets/icons/coffee-solid.svg', title: 'Coffee break' },
-    { icon: '../../../assets/icons/umbrella-solid.svg', title: 'Rainy day' },
-    { icon: '../../../assets/icons/moon-solid.svg', title: 'Party night' },
-    { icon: '../../../assets/icons/sun-solid.svg', title: 'Sunset' },
-    { icon: '../../../assets/icons/umbrella-beach-solid.svg', title: 'Relax at the beach' },
-    { icon: '../../../assets/icons/drum-solid.svg', title: 'Punk' },
+    { icon: '../../../assets/icons/coffee-solid.svg', title: 'Coffee break', image: '../../../assets/coffee-break.jpg' },
+    { icon: '../../../assets/icons/umbrella-solid.svg', title: 'Rainy day', image: '../../../assets/rainy-day.jpg' },
+    { icon: '../../../assets/icons/moon-solid.svg', title: 'Party night', image: '../../../assets/party-night.jpg' },
+    { icon: '../../../assets/icons/sun-solid.svg', title: 'Sunset', image: '../../../assets/sunset.jpg' },
+    { icon: '../../../assets/icons/umbrella-beach-solid.svg', title: 'Beach fever', image: '../../../assets/beach.jpg' },
+    { icon: '../../../assets/icons/drum-solid.svg', title: 'Punk', image: '../../../assets/punk.jpg' },
+    { icon: '../../../assets/icons/bed-solid.svg', title: 'Sleep time', image: '../../../assets/sleep.jpg' },
   ];
    
   constructor() { }
