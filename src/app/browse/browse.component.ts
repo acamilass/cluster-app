84f33a4acd3d0da70a6066ab9e6097e9e1f19977
Camila Santos <acamilass@gmail.com>
2018-10-15 14:29:46 -0300

    add input property to generate cards

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-browse',
@@ -8,6 +7,15 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 })
 export class BrowseComponent implements OnInit {
 
+  cards = [
+    { icon: '../../../assets/icons/coffee-solid.svg', title: 'Coffee break', image: '../../../assets/coffee-break.jpg' },
+    { icon: '../../../assets/icons/umbrella-solid.svg', title: 'Rainy day', image: '../../../assets/rainy-day.jpg' },
+    { icon: '../../../assets/icons/moon-solid.svg', title: 'Party night', image: '../../../assets/party-night.jpg' },
+    { icon: '../../../assets/icons/sun-solid.svg', title: 'Sunset', image: '../../../assets/sunset.jpg' },
+    { icon: '../../../assets/icons/umbrella-beach-solid.svg', title: 'Beach fever', image: '../../../assets/beach.jpg' },
+    { icon: '../../../assets/icons/drum-solid.svg', title: 'Punk', image: '../../../assets/punk.jpg' }
+  ];
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/shared/music-card/music-card.component.ts
+++ src/app/shared/music-card/music-card.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-// import { faCoffee, faUmbrella, faMoon, faSun, faUmbrellaBeach, faDrum } from '@fortawesome/free-solid-svg-icons';
-
+import { Component, OnInit, Input } from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
@@ -9,15 +7,10 @@ import * as $ from 'jquery';
   styleUrls: ['./music-card.component.scss']
 })
 export class MusicCardComponent implements OnInit {
-
-  cards = [
-    { icon: '../../../assets/icons/coffee-solid.svg', title: 'Coffee break', image: '../../../assets/coffee-break.jpg' },
-    { icon: '../../../assets/icons/umbrella-solid.svg', title: 'Rainy day', image: '../../../assets/rainy-day.jpg' },
-    { icon: '../../../assets/icons/moon-solid.svg', title: 'Party night', image: '../../../assets/party-night.jpg' },
-    { icon: '../../../assets/icons/sun-solid.svg', title: 'Sunset', image: '../../../assets/sunset.jpg' },
-    { icon: '../../../assets/icons/umbrella-beach-solid.svg', title: 'Beach fever', image: '../../../assets/beach.jpg' },
-    { icon: '../../../assets/icons/drum-solid.svg', title: 'Punk', image: '../../../assets/punk.jpg' }
-  ];
+  
+  @Input() cardIcon: any;
+  @Input() cardTitle: string;
+  @Input() cardImage: any;
    
   constructor() { }
 
